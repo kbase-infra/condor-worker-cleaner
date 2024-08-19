09d7533263510db1a3cb1ac03e15d509035c8c26
Boris <bio-boris@github.com>
2024-08-18 23:07:19 -0500

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python:3.12-slim
 WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
-CMD ["python", "/app/main.py"]
+RUN chmod +x /app/main.sh
+#CMD ["python", "/app/main.py"]
+CMD ["/app/run.sh"]
---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Infinite loop to run the Python script every 5 minutes
+while true; do
+    echo "Starting main.py..."
+    python /app/main.py
+    echo "Script execution completed. Sleeping for 5 minutes..."
+    sleep 300  # Sleep for 5 minutes (300 seconds)
+done
